526d21fcc22e3500284d79c161a8aadab9558ea4
sherna06-su <54613543+sherna06-su@users.noreply.github.com>
2020-05-19 23:56:33 -0400

    Final Project

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,18 @@
-$.ajax({
-  url: 'https://randomuser.me/api/?results=10',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-    console.log(data.results[0].name.first);
-    $("#person").append('<h1>' + data.results[0].name.first + ' ' + data.results[0].name.last + '</h1>');
-    $.each(data.results, function(key, value){
-      $('#person').append('<h1>' + value.name.first + ' ' + value.name.last + '</h1><img src="'+ value.picture.large +'" class="img-fluid rounded-circle">')
-    })
+$(document).ready(function(){
+  console.log("Hello World");
+
+
+function scrollAppear(){
+  var introText = document.querySelector('.intro-text');
+  var introPosition = introText.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.3;
+
+  if(introPosition < screenPosition){
+    introText.classList.add('intro-appear');
   }
+}
+
+  window.addEventListener('scroll',scrollAppear);
+
+
+});
